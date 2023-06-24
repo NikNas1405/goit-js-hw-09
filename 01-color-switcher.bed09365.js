ac2754ddf6f707b12db38083ef4ca59b2523148c
NikNas1405 <103371039+NikNas1405@users.noreply.github.com>
2023-06-24 18:19:05 +0000

    Deploying to gh-pages from @ NikNas1405/goit-js-hw-09@4a52f3828f0b41cdc76c1cc160a84336594e160a 🚀

---
+++ 01-color-switcher.bed09365.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body"),n=!0,a=null;t.addEventListener("click",(function(){a=setInterval((function(){return r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),n&&(t.setAttribute("disabled","disabled"),n=!1)})),e.addEventListener("click",(function(){clearInterval(a),n||(t.removeAttribute("disabled"),n=!0)}))}();
+//# sourceMappingURL=01-color-switcher.bed09365.js.map
