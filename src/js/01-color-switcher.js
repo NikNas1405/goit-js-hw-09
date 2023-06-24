4a52f3828f0b41cdc76c1cc160a84336594e160a
NikNas1405 <103371039+NikNas1405@users.noreply.github.com>
2023-06-24 21:18:16 +0300

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
 
+let buttonStartStatus = true;
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+buttonStart.addEventListener('click', handleButtonStartClick);
+
+function handleButtonStartClick() {
+  // установить интервал
+  // сделать колбек в виде функции, иниц.рандом цвет
+  // навесить клас дисплей нон
+
+  const changeBgColor = () =>
+    (bodyEl.style.backgroundColor = getRandomHexColor());
+
+  intervalId = setInterval(changeBgColor, 1000);
+  if (buttonStartStatus) {
+    buttonStart.setAttribute('disabled', 'disabled');
+    buttonStartStatus = false;
+  }
+}
+
+buttonStop.addEventListener('click', handleButtonStopClick);
+
+function handleButtonStopClick() {
+  // снять клас дисплей нон
+  // остановить функцию getRandomHexColor
+  // снять интервал
+
+  clearInterval(intervalId);
+  if (!buttonStartStatus) {
+    buttonStart.removeAttribute('disabled');
+    buttonStartStatus = true;
+  }
+}
