86e94c1d036b36a02aa9c11422fd0b0eccab4142
NikNas1405 <103371039+NikNas1405@users.noreply.github.com>
2023-06-25 10:14:12 +0000

    Deploying to gh-pages from @ NikNas1405/goit-js-hw-09@74f23df5f5d8a4fb35fa1d4fa74d0f210bc00561 🚀

--- 01-color-switcher.b3400ffc.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let a=!0,d=null;t.addEventListener("click",(function(){d=setInterval((()=>r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`),1e3),a&&(t.setAttribute("disabled","disabled"),a=!1)})),e.addEventListener("click",(function(){clearInterval(d),a||(t.removeAttribute("disabled"),a=!0)}));
-//# sourceMappingURL=01-color-switcher.b3400ffc.js.map
