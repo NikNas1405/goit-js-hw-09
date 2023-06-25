320000238ff5a65fc95088640df22c29126c4701
NikNas1405 <103371039+NikNas1405@users.noreply.github.com>
2023-06-25 05:59:33 +0000

    Deploying to gh-pages from @ NikNas1405/goit-js-hw-09@bfb5a723953fcb21f34737d6a83c6827a283ab5d 🚀

--- 03-promises.62da2cb0.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("form"),{delay:o,step:n,amount:t}=e;function i(e,o){Math.random()}e.addEventListener("submit",(function(e){e.preventDefault(),i(t.value)})),i().then((({position:e,delay:o})=>{console.log(`✅ Fulfilled promise ${e} in ${o}ms`)})).catch((({position:e,delay:o})=>{console.log(`❌ Rejected promise ${e} in ${o}ms`)}));
-//# sourceMappingURL=03-promises.62da2cb0.js.map
